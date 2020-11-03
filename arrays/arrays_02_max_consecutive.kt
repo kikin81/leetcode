8b8df6f55be6f998e936a46c6c83d0aedcdb0752
Francisco Velazquez <francisco.velazquez@avast.com>
2020-11-02 18:54:39 -0800

    added comments

--- arrays/arrays_02_max_consecutive.kt
+++ arrays/arrays_02_max_consecutive.kt
@@ -1,10 +1,19 @@
+/**
+* Max consecutive ones problem from leetcode
+* https://leetcode.com/explore/featured/card/fun-with-arrays/521/introduction/3238/
+*/
 fun main(args: Array<String>) {
     var inputArr = intArrayOf(1, 0, 1, 1, 0, 1)
     val result = findMaxConsecutiveOnes(inputArr)
 
+    // expected 2
     print(result)
 }
 
+/**
+* Find the maxinum number of consecutive ones
+* in an array of 0 and 1s
+*/
 fun findMaxConsecutiveOnes(nums: IntArray): Int {
     var maxC = 0
     var currentC = 0
